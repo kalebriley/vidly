303378c1aef1d8d3ce1f15d007552b27da888043
Kaleb Riley <Kaleb.riley91@gmail.com>
2019-09-01 23:20:41 -0600

    add static hosting

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 // configure middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(express.static('./src/public'))
 
 // configure routes
 app.use('/api/genres', genres)
