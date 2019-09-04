f2f927a9cd8540fdafc7f4f857a0488bbb121eb3
Kaleb Riley <Kaleb.riley91@gmail.com>
2019-09-03 20:12:21 -0600

    add database connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet')
 const morgan = require('morgan')
 const config = require('config')
+const Mongoose = require('mongoose')
 const startupDebugger = require('debug')('app:startup')
 const routes = require('./src/routes')
 
@@ -19,6 +20,12 @@ if (app.get('env') === 'development') {
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(helmet())
+
+// Connect to data base
+const db = `mongodb+srv://${config.get('db.db_username')}:${config.get('db.db_password')}@cluster0-sspue.mongodb.net/main?retryWrites=true&w=majority`
+Mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(ex => console.log('MongoDB failled to connect...'))
 
 // configure view engine
 app.set('view engine', 'pug')
